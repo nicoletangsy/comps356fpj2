1a438784563c2a333438b12b668398597cc21e8a
RaicKez <raiden8639@yahoo.com.hk>
2018-04-04 15:29:57 +0800

    update profile.html and profile.php

---
+++ profile.php
@@ -0,0 +1,24 @@
+<?php
+	require_once('database.php');
+	session_start();
+
+	$url = $_POST['url'];
+	$gender = $_POST['gender'];
+	$bday = $_POST['bday'];
+	$religious = $_POST['religious'];
+	$selfintro = $_POST['selfintro'];
+	try{
+
+	$sql = "INSERT INTO profile (url,gender,month,religious,selfintro)
+	VALUES ('".$url."','".$gender."','".$bday."','".$religious."','".$selfintro."')";
+
+
+	$conn->exec($sql);
+    echo "New record created successfully";
+    header("Refresh:1,url=profile.html");
+    }
+	catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+?>
