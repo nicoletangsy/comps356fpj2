59031b720a6496b3075d91fa4d6931dd29f66190
tszholaw <g1166179@study.ouhk.edu.hk>
2018-04-23 13:33:29 +0800

    update report function

---
+++ report.php
@@ -0,0 +1,34 @@
+<?php
+$conn = new mysqli('localhost','root','root','cellfish');
+session_start();
+$d=strtotime("+8 Hours");
+$date=date('Y-m-d H:i:s', $d);
+
+if (isset($_SESSION['username'])){
+
+if(isset($_GET['reid'], $_GET['board_id'])){
+    $user = (string)$_SESSION['username'];
+    $reportid = (int)$_GET['reid'];
+    $reportpost_id = (int)$_GET['board_id'];
+
+        $exists = $conn->query("SELECT id FROM board WHERE id = {$reportpost_id}")->num_rows ? true : false;
+        $existsusername = $conn->query("SELECT report_user FROM reportpost WHERE report_user = '{$user}' AND reportpost_id = {$reportpost_id}")->num_rows ? true : false;
+
+        if($existsusername){
+            if($exists){
+            $conn->query("UPDATE reportpost SET reason = {$reportid} ,report_date = '{$date}' WHERE report_user = '{$user}' AND reportpost_id = {$reportpost_id}");
+            header('Location: discussboard.php');
+            }
+        }else{
+            if($exists){
+                $conn->query("INSERT INTO reportpost (report_user,report_date,reportpost_id,reason) VALUES ('{$user}','{$date}',{$reportpost_id},'{$reportid}')");
+                header('Location: discussboard.php');
+        }
+    }
+}
+}
+else{
+    die("you need to <a href =\"login.php\">login</a> first!<br><br>Return to the <a href =\"index.php\">Homepage.</a>");
+}
+
+?>
---
+++ reportcomment.php
@@ -0,0 +1,34 @@
+<?php
+$conn = new mysqli('localhost','root','root','cellfish');
+session_start();
+$d=strtotime("+8 Hours");
+$date=date('Y-m-d H:i:s', $d);
+
+if (isset($_SESSION['username'])){
+
+if(isset($_GET['reid'], $_GET['reply_id'])){
+    $user = (string)$_SESSION['username'];
+    $reportid = (int)$_GET['reid'];
+    $reportcomment_id = (int)$_GET['reply_id'];
+
+        $exists = $conn->query("SELECT reply_id FROM replyboard WHERE reply_id = {$reportcomment_id}")->num_rows ? true : false;
+        $existsusername = $conn->query("SELECT report_user FROM reportcomment WHERE report_user = '{$user}' AND reportcomment_id = {$reportcomment_id}")->num_rows ? true : false;
+
+        if($existsusername){
+            if($exists){
+            $conn->query("UPDATE reportcomment SET reason = {$reportid} ,report_date = '{$date}' WHERE report_user = '{$user}' AND reportcomment_id = {$reportcomment_id}");
+            header('Location: discussboard.php');
+            }
+        }else{
+            if($exists){
+                $conn->query("INSERT INTO reportcomment (report_user,report_date,reportcomment_id,reason) VALUES ('{$user}','{$date}',{$reportcomment_id},'{$reportid}')");
+                header('Location: discussboard.php');
+        }
+    }
+}
+}
+else{
+    die("you need to <a href =\"login.php\">login</a> first!<br><br>Return to the <a href =\"index.php\">Homepage.</a>");
+}
+
+?>
